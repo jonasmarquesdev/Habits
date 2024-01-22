3d9eebd412c5a233772b2a92abece1c26ac2995c
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2024-01-22 18:28:38 -0300

    habit completion route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -79,4 +79,56 @@ export async function appRoutes(app: FastifyInstance) {
       completedHabits,
     }
   });
+
+  // complete or not complete a habit
+  app.patch('/habits/:id/toggle', async (request) => {
+
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = toggleHabitParams.parse(request.params)
+
+    const today = dayjs().startOf('day').toDate();
+    
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      }
+    })
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        }
+      })
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        }
+      }
+    })
+
+    if (dayHabit) {
+      // remove mark from complete
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        }
+      })
+    } else {
+      // mark as complete
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        }
+      })
+    }
+  });
 }
