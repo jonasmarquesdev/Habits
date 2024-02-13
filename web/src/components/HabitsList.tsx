e9110475706338355ac35cc8e8b3fae25d0b12d1
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2024-02-13 13:47:44 -0300

    disabling in past dates

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -2,6 +2,7 @@ import * as Checkbox from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
 import { useEffect, useState } from "react";
 import { api } from "../lib/axios";
+import dayjs from "dayjs";
 
 interface HabitsListProps {
   date: Date;
@@ -31,13 +32,18 @@ export function HabitsList({ date }: HabitsListProps) {
       });
   }, []);
 
+  const isDateInPast = dayjs(date)
+    .endOf("day")
+    .isBefore(new Date());
+
   return (
     <div className="mt-6 flex flex-col gap-3">
       {habitsInfo?.possibleHabits.map((habit) => {
         return (
-          <Checkbox.Root 
+          <Checkbox.Root
             key={habit.id}
             checked={habitsInfo.completedHabits.includes(habit.id)}
+            disabled={isDateInPast}
             className="flex items-center gap-3 group"
           >
             <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
